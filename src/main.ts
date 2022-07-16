4a83b8b97b388242fbd05913a897e289039adee2
Jim Lawruk <jimlawruk@yahoo.com>
2022-07-16 12:04:32 -0400

    Removed unused variable

--- src/main.ts
+++ src/main.ts
@@ -18,19 +18,19 @@ player.activities = [Activities.Activity1, Activities.Activity2];
 const start = player.activities[0].points[0];
 
 const map = new Map(
-  { basemap: "hybrid" }
+    { basemap: "hybrid" }
 );
 
 const view = new MapView({
-  map: map,
-  container: 'viewDiv',
-  center: start,
-  zoom: 16
+    map: map,
+    container: 'viewDiv',
+    center: start,
+    zoom: 16
 });
 
 const basemapToggle = new BasemapToggle({
-  view: view,
-  nextBasemap: 'streets-vector'
+    view: view,
+    nextBasemap: 'streets-vector'
 });
 view.ui.add(basemapToggle, "bottom-left");
 
@@ -38,28 +38,28 @@ const pointLayer = new GraphicsLayer({});
 map.add(pointLayer);
 
 let colors = [
-  [200, 0, 0],
-  [200, 200, 0],
-  [0, 0, 200],
-  [200, 0, 200]
+    [200, 0, 0],
+    [200, 200, 0],
+    [0, 0, 200],
+    [200, 0, 200]
 ];
 
 let setActivityText = () => {
-  let html = '';
-  for (let i = 0; i < player.activities.length; i++) {
-    let activity = player.activities[i];
-    if (activity.accumulatedDistance === undefined) {
-      activity.accumulatedDistance = 0;
-      activity.averagePace = '';
-    }
-    html += `<tr>
+    let html = '';
+    for (let i = 0; i < player.activities.length; i++) {
+        let activity = player.activities[i];
+        if (activity.accumulatedDistance === undefined) {
+            activity.accumulatedDistance = 0;
+            activity.averagePace = '';
+        }
+        html += `<tr>
                 <td class="icon"><span style="background-color: rgb(${colors[i].join(',')})"></td>
                 <td>${activity.title}</td>
                 <td>${activity.accumulatedDistance.toFixed(2)}</td>
                 <td>${activity.averagePace}</td>
             </tr>`;
-  }
-  html = `<table class="table">
+    }
+    html = `<table class="table">
             <thead>
               <th></th>
               <th>Activity Name</th>
@@ -68,123 +68,121 @@ let setActivityText = () => {
             </thhead>
             <tbody>${html}</tboday>
           </table>`;
-  (<any>document.getElementById('activities')).innerHTML = html;
+    (<any>document.getElementById('activities')).innerHTML = html;
 };
 
 let setSpeedText = () => {
-  const multiplierElement = document.querySelector<HTMLDivElement>('#multiplier');
-  (<any>multiplierElement).innerHTML = `${player.multiplier}`;
+    const multiplierElement = document.querySelector<HTMLDivElement>('#multiplier');
+    (<any>multiplierElement).innerHTML = `${player.multiplier}`;
 };
 
 let setStartPauseText = () => {
-  document.getElementById('start')?.style.setProperty('display', player.paused ? 'inline-block' : 'none');
-  document.getElementById('pause')?.style.setProperty('display', !player.paused ? 'inline-block' : 'none');
+    document.getElementById('start')?.style.setProperty('display', player.paused ? 'inline-block' : 'none');
+    document.getElementById('pause')?.style.setProperty('display', !player.paused ? 'inline-block' : 'none');
 };
 
 const setTimeText = () => {
-  const timeElement = document.querySelector<HTMLDivElement>('#time');
-  (<any>timeElement).innerHTML = player.getMinutesSeconds(player.seconds)
+    const timeElement = document.querySelector<HTMLDivElement>('#time');
+    (<any>timeElement).innerHTML = player.getMinutesSeconds(player.seconds)
 };
 
 let refreshGraphics = () => {
-  pointLayer.removeAll();
-  let pointAdded = false;
-  for (let i = 0; i < player.activities.length; i++) {
-    let activity = player.activities[i];
-    if (activity.points?.length > player.seconds) {
-      const graphic = MapUtils.getPointGraphic(activity.points[player.seconds], colors[i]);
-      pointLayer.add(graphic);
-      pointAdded = true;
+    pointLayer.removeAll();
+    for (let i = 0; i < player.activities.length; i++) {
+        let activity = player.activities[i];
+        if (activity.points?.length > player.seconds) {
+            const graphic = MapUtils.getPointGraphic(activity.points[player.seconds], colors[i]);
+            pointLayer.add(graphic);
+        }
     }
-  }
 }
 
 let refresh = () => {
-  refreshGraphics();
-  setActivityText();
-  setStartPauseText();
-  setTimeText();
-  setSpeedText();
+    refreshGraphics();
+    setActivityText();
+    setStartPauseText();
+    setTimeText();
+    setSpeedText();
 }
 
 const center = () => {
-  if (player.activities.length && player.activities[0].points.length > player.seconds) {
-    (<any>view).center = player.activities[0].points[player.seconds];
-  }
-  player.calculateDistances();
-  refresh();
+    if (player.activities.length && player.activities[0].points.length > player.seconds) {
+        (<any>view).center = player.activities[0].points[player.seconds];
+    }
+    player.calculateDistances();
+    refresh();
 }
 
 const createActivityFromTextResult = (textResult: any) => {
-  var activity = gpxParser.getActivitiesFromResult(textResult);
-  player.activities.push(activity);
-  player.seconds = 0;
-  refresh();
-  if (player.activities.length === 1) {
-    center();
-  }
+    var activity = gpxParser.getActivitiesFromResult(textResult);
+    player.activities.push(activity);
+    player.seconds = 0;
+    refresh();
+    if (player.activities.length === 1) {
+        center();
+    }
 };
 
 document.addEventListener('player-tick', () => {
-  refresh(); 
+    refresh();
 });
 
 document.getElementById('clear')?.addEventListener('click', () => {
-  player.clearActivities();
-  refresh();
+    player.clearActivities();
+    refresh();
 });
 
 document.getElementById('reset')?.addEventListener('click', () => {
-  player.toggleStartPause(true);
-  player.seconds = 0;
-  refresh();
+    player.toggleStartPause(true);
+    player.seconds = 0;
+    refresh();
 });
 
 document.getElementById('slower')?.addEventListener('click', () => {
-  player.adjustSpeed(false);  
-  refresh();
+    player.adjustSpeed(false);
+    refresh();
 });
 
 document.getElementById('faster')?.addEventListener('click', () => {
-  player.adjustSpeed(true);
-  refresh();
+    player.adjustSpeed(true);
+    refresh();
 });
 
 document.getElementById('back')?.addEventListener('click', () => {
-  player.seconds = player.seconds - player.multiplier;
-  refresh();
+    player.seconds = player.seconds - player.multiplier;
+    refresh();
 });
 
 document.getElementById('forward')?.addEventListener('click', () => {
-  player.seconds = player.seconds + player.multiplier;
-  refresh();
+    player.seconds = player.seconds + player.multiplier;
+    refresh();
 });
 
 document.getElementById('pause')?.addEventListener('click', () => {
-  player.toggleStartPause(true);
-  refresh();
+    player.toggleStartPause(true);
+    refresh();
 });
 
 document.getElementById('start')?.addEventListener('click', () => {
-  player.toggleStartPause(false);
-  refresh();
+    player.toggleStartPause(false);
+    refresh();
 });
 
 document.getElementById('center')?.addEventListener('click', () => {
-  center();
+    center();
 });
 
 document.getElementById("gpxFile")?.addEventListener('change', () => {
-  let reader = new FileReader();
-  reader.addEventListener("load", () => {
-    createActivityFromTextResult(reader.result);
-  }, false);
-  const files = (document.getElementById('gpxFile') as HTMLInputElement).files;
-  if (files) {
-    for (let i = 0; i < files.length; i++) {
-      reader.readAsText(files[i]);
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+        createActivityFromTextResult(reader.result);
+    }, false);
+    const files = (document.getElementById('gpxFile') as HTMLInputElement).files;
+    if (files) {
+        for (let i = 0; i < files.length; i++) {
+            reader.readAsText(files[i]);
+        }
     }
-  }
 });
 
 player.restartTimer();
