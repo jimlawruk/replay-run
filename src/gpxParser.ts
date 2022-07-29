4436e48e6367da6051bc05bc3d8277e4b5356bdf
Jim Lawruk <jimlawruk@yahoo.com>
2022-07-29 10:37:15 -0400

    Improved GPX parser to use DomParser

--- src/gpxParser.ts
+++ src/gpxParser.ts
@@ -2,19 +2,39 @@ import { Activity } from "./models";
 
 export class GPXParser {
 
+    domParser = new DOMParser();
+
     getActivitiesFromResult(fileText: string): Activity {
-        const lines: string[] = fileText.split('\n');
+        const xmlDoc = this.domParser.parseFromString(fileText, 'text/xml');
+        const trkPoints = xmlDoc.getElementsByTagName('trkpt');
         const longLatArray: Array<Array<number>> = [];
-        let title = 'New Activity';
-        for (const line of lines) {
-            if (line.indexOf('<trkpt lat="') >= 0) {
-                const values = line.trim().replace('<trkpt lat="', '').replace('" lon="', ',').replace('">', '').split(',');
-                longLatArray.push([parseFloat(values[1]), parseFloat(values[0])]);
-            } else if (line.indexOf('<name>') >= 0) {
-                title = line.replace('<name>', '').replace('</name>', '').trim();
+        let intervalTime = 1;
+        const intervalTimeElements = xmlDoc.getElementsByTagName('intervalTime');
+        if (intervalTimeElements.length > 0) {
+            intervalTime = parseInt(intervalTimeElements[0].innerHTML);
+        }
+        let title = xmlDoc.getElementsByTagName('name')[0].innerHTML;
+        for (let i = 0; i < trkPoints.length; i++) {
+            const lon = this.getFloatVal(trkPoints[i], 'lon');
+            const lat = this.getFloatVal(trkPoints[i], 'lat');
+            if (i === 0) {
+                longLatArray.push([lon, lat]);
+            } else {
+                for (let j = 0; j < intervalTime; j++) {
+                    longLatArray.push([lon, lat]);
+                }
             }
         }
-        return { title: title, points: longLatArray }
+        return { title: title, points: longLatArray };
+    }
+
+    getFloatVal(element: Element, key: string) {
+        const value = this.getAttrVal(element, key);
+        return parseFloat(value);
+    }
+
+    getAttrVal(element: Element, key: string): string {
+        return element.attributes.getNamedItem(key)?.value || '';
     }
 
 }
