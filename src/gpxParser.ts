cff50a2f89b69a292584a409f930bc419ae7a8e4
Jim Lawruk <jlawruk@geodecisions.com>
2023-11-09 09:29:26 -0500

    Take into account time elements if they exist

--- src/gpxParser.ts
+++ src/gpxParser.ts
@@ -14,9 +14,14 @@ export class GPXParser {
             intervalTime = parseInt(intervalTimeElements[0].innerHTML);
         }
         let title = xmlDoc.getElementsByTagName('name')[0].innerHTML;
+        let previousTime: Date | null = null;
         for (let i = 0; i < trkPoints.length; i++) {
             const lon = this.getFloatVal(trkPoints[i], 'lon');
             const lat = this.getFloatVal(trkPoints[i], 'lat');
+            const time = this.getTimeVal(trkPoints[i]);
+            if (previousTime && time) {
+                intervalTime = (time!.valueOf() - previousTime!.valueOf()) / 1000;
+            }
             if (i === 0) {
                 longLatArray.push([lon, lat]);
             } else {
@@ -24,6 +29,7 @@ export class GPXParser {
                     longLatArray.push([lon, lat]);
                 }
             }
+            previousTime = time;
         }
         return { title: title, points: longLatArray, visible: true };
     }
@@ -35,6 +41,25 @@ export class GPXParser {
 
     getAttrVal(element: Element, key: string): string {
         return element.attributes.getNamedItem(key)?.value || '';
+    }
+
+    getTimeVal(element: Element): Date | null {
+        const timeValue = this.getChildElementValue(element, "time");
+        if (timeValue) {
+            return new Date(timeValue);
+        }
+        return null;
+    }
+
+    getChildElementValue(element: Element, key: string): string | null {
+        let i = 0;
+        for (i = 0; i < element.childNodes.length; i++) {
+            let node = element.childNodes[i];
+            if (node.nodeName === key) {
+                return (<any>node).innerHTML;
+            }
+        }        
+        return null;
     }
 
 }
